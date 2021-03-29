f094dfb00f9a99bc6583b4a88829819a7977b82b
deyou <chinadeyou@qq.com>
2021-03-30 01:23:49 +0800

    add hello module fops

--- hello_world/hello_world.c
+++ hello_world/hello_world.c
@@ -7,10 +7,57 @@
 
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/cdev.h>
+#include <linux/fs.h>
+
+#define HELLO_DEV_MINOR	0
+#define HELLO_NR_DEVS	4
+
+int hello_major;
+int hello_minor = 0;
+
+dev_t dev_no;
+
+int hello_open(struct inode *inode, struct file *filp)
+{
+	return 0;
+}
+
+int hello_release(struct inode *inode, struct file *filp)
+{
+	return 0;
+}
+
+ssize_t hello_read(struct file *filp, char __user *addr, size_t size, loff_t *offset)
+{
+	return 0;
+}
+
+ssize_t hello_write(struct file *filp, const char __user *addr, size_t size, loff_t *offset)
+{
+	return 0;
+}
+
+struct file_operations hello_fops = {
+	.open = hello_open,
+	.read = hello_read,
+	.write = hello_write,
+	.release = hello_release,
+};
 
 static int __init hello_world_init(void)
 {
+
 	printk("hello world\n");
+	/* alloc device num */
+	alloc_chrdev_region(&dev_no, hello_minor, HELLO_NR_DEVS, "hello world");
+
+	printk("alloc dev no is %d\n", dev_no);
+
+//	struct cdev hello;
+
+
+
 
 	return 0;
 }
@@ -18,6 +65,8 @@ static int __init hello_world_init(void)
 
 static void __exit hello_world_exit(void)
 {
+	unregister_chrdev_region(dev_no, HELLO_NR_DEVS);
+	printk("unregister dev no is %d\n", dev_no);
 	printk("GoodBye\n");
 }
 
